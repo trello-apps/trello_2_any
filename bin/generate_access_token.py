5914176c3206218e95b86876466039354277e366
Ali AYOUBI <ali.m.ayoubi@gmail.com>
2017-04-16 20:26:57 +0000

    Generate access token is now compatible with py3

--- bin/generate_access_token.py
+++ bin/generate_access_token.py
@@ -8,7 +8,7 @@ parser.add_argument("--expiration", help="the token's expieration, defaults to 3
 parser.add_argument("--write-access", help="the token has write access", default=False,action="store_true")
 args = vars(parser.parse_args())
 
-print("Access the following link:")
+print("Access the following link to generate your access token:")
 trello = TrelloApi(args['client_api_key'])
-print trello.get_token_url(args['app_name'], expires=args['expiration'], write_access=args['write_access'])
+print(trello.get_token_url(args['app_name'], expires=args['expiration'], write_access=args['write_access']))
 
